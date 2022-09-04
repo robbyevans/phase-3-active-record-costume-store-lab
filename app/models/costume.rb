45e27928bfc9fdddd7b5f5f069c69ae94442068c
Evans Rutto <robbyevans001@gmail.com>
2022-09-04 14:06:07 +0300

    commit tree 60160e0440ab3390574e7d209df2833f469854a6

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,14 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+  # def change
+  #   create_table :constumes do |t|
+  #     t.string :name
+  #     t.float :price
+  #     t.string :size
+  #     t.string :image_url
+  #     t.timestamp :created_at
+  #     t.timestamp :updated_at
+  #   end
+  # end
+end
